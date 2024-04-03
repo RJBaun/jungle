5894fc577df30c3ca5a389b95e59b607002b173b
Rylan Baun <rylanjbaun@gmail.com>
2024-04-03 17:26:25 -0600

    add cards to dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,8 @@ class Admin::DashboardController < ApplicationController
   before_action :authenticate
 
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
 
   def authenticate
